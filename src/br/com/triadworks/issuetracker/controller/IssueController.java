12f856e4dcb8b3b2154f1f9e9ebd2d0402dca9c2
Rafael Ponte <rponte@gmail.com>
2012-11-12 11:35:26 -0300

    Remove validação server-side do Cadastro de Issues;

--- src/br/com/triadworks/issuetracker/controller/IssueController.java
+++ src/br/com/triadworks/issuetracker/controller/IssueController.java
@@ -1,11 +1,8 @@
 package br.com.triadworks.issuetracker.controller;
 
 import java.util.List;
-
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
-import br.com.caelum.vraptor.Validator;
-import br.com.caelum.vraptor.validator.ValidationMessage;
 import br.com.triadworks.issuetracker.dao.IssueDao;
 import br.com.triadworks.issuetracker.dao.ProjetoDao;
 import br.com.triadworks.issuetracker.dao.UsuarioDao;
@@ -15,17 +12,15 @@ import br.com.triadworks.issuetracker.model.Issue;
 public class IssueController {
 	
 	private final IssueDao dao;
-	private final Validator validator;
 	private final Result result;
 	private final ProjetoDao projetoDao;
 	private final UsuarioDao usuarioDao;
 
 	public IssueController(IssueDao dao, ProjetoDao projetoDao, 
-			UsuarioDao usuarioDao, Validator validator, Result result) {
+			UsuarioDao usuarioDao, Result result) {
 		this.dao = dao;
 		this.projetoDao = projetoDao;
 		this.usuarioDao = usuarioDao;
-		this.validator = validator;
 		this.result = result;
 	}
 	
@@ -40,10 +35,6 @@ public class IssueController {
 	}
 	
 	public void adiciona(Issue issue) {
-		
-		valida(issue);
-		validator.onErrorUsePageOf(this).novo();
-		
 		dao.salva(issue);
 		result.include("notice", "Issue adicionada com sucesso!");
 		result.redirectTo(this).lista();
@@ -56,10 +47,6 @@ public class IssueController {
 	}
 	
 	public void altera(Issue issue) {
-		
-		valida(issue);
-		validator.onErrorUsePageOf(this).edita(null);
-		
 		dao.atualiza(issue);
 		result.include("notice", "Issue atualizada com sucesso!");
 		result.redirectTo(this).lista();
@@ -70,31 +57,6 @@ public class IssueController {
 		dao.remove(issue);
 		result.include("notice", "Issue removida com sucesso!");
 		result.redirectTo(this).lista();
-	}
-	
-	private void valida(Issue issue) {
-		if (issue.getSumario() == null
-				|| issue.getSumario().length() < 3) {
-			validator.add(new ValidationMessage("Campo obrigatório e precisa ter mais de 3 letras", "Sumário"));
-		}
-		if (issue.getDescricao() == null
-				|| issue.getDescricao().isEmpty()) {
-			validator.add(new ValidationMessage("Campo obrigatório", "Descrição"));
-		}
-		if (issue.getProjeto() == null
-				|| issue.getProjeto().getId() == null) {
-			validator.add(new ValidationMessage("Campo obrigatório", "Projeto"));
-		}
-		if (issue.getTipo() == null) {
-			validator.add(new ValidationMessage("Campo obrigatório", "Tipo"));
-		}
-		if (issue.getReportadoEm() == null) {
-			validator.add(new ValidationMessage("Campo obrigatório", "Reportado em"));
-		}
-		if (issue.getAssinadoPara() == null
-				|| issue.getAssinadoPara().getId() == null) {
-			validator.add(new ValidationMessage("Campo obrigatório", "Assinado para"));
-		}
 	}
 	
 }
