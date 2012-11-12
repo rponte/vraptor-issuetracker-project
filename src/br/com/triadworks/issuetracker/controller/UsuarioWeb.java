4528af939df8a1955368ee1836f12b9fc2032a41
Rafael Ponte <rponte@gmail.com>
2012-11-12 10:33:31 -0300

    Torna UsuarioWeb serializável;

--- src/br/com/triadworks/issuetracker/controller/UsuarioWeb.java
+++ src/br/com/triadworks/issuetracker/controller/UsuarioWeb.java
@@ -1,13 +1,17 @@
 package br.com.triadworks.issuetracker.controller;
 
+import java.io.Serializable;
+
 import br.com.caelum.vraptor.ioc.Component;
 import br.com.caelum.vraptor.ioc.SessionScoped;
 import br.com.triadworks.issuetracker.model.Usuario;
 
 @Component
 @SessionScoped
-public class UsuarioWeb {
+public class UsuarioWeb implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+	
 	private Usuario usuario;
 	
 	public void loga(Usuario usuario) {
